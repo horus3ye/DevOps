91fab61cd9b6d4225e781060ba88acc283b28bba
megzDevOps <mnmagdy2020@gmail.com>
2022-02-13 13:04:00 +0300

    adding the counter

---
+++ deadline counter.py
@@ -0,0 +1,20 @@
+from datetime import datetime as dt
+
+print("Give us your goal and the deadline sparated by a colon :)")
+goal_deadline = input().split(":")
+    
+goal_var = goal_deadline[0]
+dealine = goal_deadline[1]
+deadline = dt.strptime(dealine, "%d-%m-%Y")
+tody_dte = dt.today()
+time_rem = deadline - tody_dte
+
+if time_rem.days <= 3:
+    print(f"You Still have {int(time_rem.total_seconds() / 60 / 60)} hours to {goal_var} deadline. Do Your best ^_^")
+elif time_rem.days <= 30:
+    print(f"You Still have {time_rem.days} days to {goal_var} deadline. Do Your best ^_^")
+elif time_rem.days > 30:
+    time_rem_mon = time_rem.days / 30
+    time_rem_mod = time_rem.days % 30
+    print(f"You Still have {int(time_rem_mon)} months and {time_rem_mod} days to make your {goal_var} deadline. Do Your best ^_^")
+    
